7fe15bd3145afa25ce5be2f05d0e0f1848f42ccd
ZairBulos <buloszair@gmail.com>
2024-04-13 12:19:05 -0300

    feat: add Single Responsibility Principle

---
+++ src/SingleResponsibilityPrinciple/Corrected/services/todos.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import type { TodoType } from "../types/todo";
+
+export const fetchTodo = () => {
+  return axios
+    .get<TodoType[]>(`https://jsonplaceholder.typicode.com/todos`)
+    .then((res) => res.data);
+};
