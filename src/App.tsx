b79aee01c64045464c78d3058f01fa2dcc204cb1
ZairBulos <buloszair@gmail.com>
2024-04-13 12:08:19 -0300

    feat: add initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Principios SOLID en React JS</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/jKdt-BnTTR0?si=hBoOGM1RAQ0i7Cvj"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
